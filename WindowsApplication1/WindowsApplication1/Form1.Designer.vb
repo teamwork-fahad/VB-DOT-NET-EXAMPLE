5bd461f01382307209aeff6f247e7c2e9df417ac
Fahad <fahadvohra143@gmail.com>
2024-01-03 16:10:33 +0530

    Combo box example

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbState = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmbCity = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmbState
+        '
+        Me.cmbState.FormattingEnabled = True
+        Me.cmbState.Items.AddRange(New Object() {"GUJ", "RJ", "MH"})
+        Me.cmbState.Location = New System.Drawing.Point(184, 152)
+        Me.cmbState.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.cmbState.Name = "cmbState"
+        Me.cmbState.Size = New System.Drawing.Size(261, 29)
+        Me.cmbState.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(219, 105)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(187, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Example of Combobox"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 44)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 29)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Add Value"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cmbCity
+        '
+        Me.cmbCity.FormattingEnabled = True
+        Me.cmbCity.Location = New System.Drawing.Point(184, 213)
+        Me.cmbCity.Name = "cmbCity"
+        Me.cmbCity.Size = New System.Drawing.Size(261, 29)
+        Me.cmbCity.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(61, 291)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(117, 53)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Count State"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(184, 291)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(121, 53)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Count City"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(473, 422)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.cmbCity)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbState)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte), True)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmbState As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents cmbCity As System.Windows.Forms.ComboBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim val As String
+        val = TextBox1.Text
+        cmbState.Items.Add(val)
+        MessageBox.Show(val & "state is added....")
+        TextBox1.Text = "" 'empty or clear textbox value
+    End Sub
+
+    Private Sub cmbState_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbState.SelectedIndexChanged
+        cmbCity.Items.Clear() 'clear all
+
+        Dim state As String
+        state = cmbState.SelectedItem.ToString
+        If state = "GUJ" Then
+            cmbCity.Items.Add("Surat")
+            cmbCity.Items.Add("Rajkot")
+            cmbCity.Items.Add("Vadodra")
+            cmbCity.Items.Add("Ahemdabad")
+        ElseIf state = "MH" Then
+            cmbCity.Items.Add("Mumbai")
+            cmbCity.Items.Add("Pune")
+            cmbCity.Items.Add("Lonavala")
+        ElseIf state = "RJ" Then
+            cmbCity.Items.Add("Jaipur")
+            cmbCity.Items.Add("Jodhpur")
+            cmbCity.Items.Add("Jaisailmair")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim cs As Integer
+        cs = cmbState.Items.Count
+        MsgBox("Total state is " & cs)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim cc As Integer
+        cc = cmbCity.Items.Count
+        MsgBox("Total city is " & cc)
+    End Sub
+End Class
