9473a3cf7a0d615f09548a1530a291c09a6f5dd7
Fahad <fahadvohra143@gmail.com>
2024-02-03 11:23:07 +0530

    Dice Game

---
+++ DICE_GAME/DICE_GAME/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        Button1 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(219, 75)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(197, 131)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(216, 285)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(200, 35)
+        Button1.TabIndex = 1
+        Button1.Text = "Rolling the Dice"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ DICE_GAME/DICE_GAME/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+        n = Int(1 + Rnd() * 6)
+        Dim str As String = "D:\Fahad Sir\VB APP\DICE_GAME\DICE_GAME\d" & n & ".png"
+
+        PictureBox1.Image = Image.FromFile(str)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.Height = 200
+        PictureBox1.Width = 200
+    End Sub
+End Class
